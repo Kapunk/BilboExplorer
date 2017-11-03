b35d1fee899c1e60b3012561658f6a312592d90c
Kapunk <kepa10@gmail.com>
2017-11-03 18:31:54 +0100

    modificaciones

--- motor/datos/datos.py
+++ motor/datos/datos.py
@@ -1,3 +1,23 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+	BilboExplorer - Descubre Bilbao!
+    Copyright (C) 2017  Kepa Muñoz (kepa10@gmail.com)
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 
 urlAgendaBilbao = 'http://www.bilbao.eus/cs/Satellite?c=Page&cid=1272990237857&pageid=1272990237857&idioma=es&pagename=Bilbaonet/Page/BIO_ListadoEventosAppInfoBilbao&todos=si'
